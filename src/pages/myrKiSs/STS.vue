57bf6a6d8019a10711cc31d4de89563c32b45b05
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-18 03:21:14 -0500

    Add STS to STS

---
+++ src/pages/myrKiSs/STS.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- <a href="https://sonic-circle.netlify.app/" target="_blank"> -->
+    <img class="card" src="https://madamadam.s3.us-east-2.amazonaws.com/STS-3ff9.png">
+    <!-- </a> -->
+  </div>
+</template>
+
+<style>
+  .card {
+    max-width: 50%;
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    border: 5px;
+    border-color: white;
+    border-style: solid;
+    border-radius: 1%;
+  }
+</style>
